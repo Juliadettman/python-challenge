44709056da1dafbaceb8a6d80f1708a6de49a4f4
Juliadettman <143463072+Juliadettman@users.noreply.github.com>
2023-09-28 18:52:58 -0500

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,9 +6,11 @@ import csv
 budget_csv = os.path.join( 'PyBank2', 'Resources', 'budget_data.csv')
 # Initialize variables
 total_months = 0 
-total_profit = 0
-previous_profit = 0
-profit_changes = []
+total_profit_loss = 0
+prev_profit_loss = 0
+profit_loss_change = 0
+greatest_increase = ["", 0]
+greatest_decrease = ["", 99999999999999999]
 
 # Open and read CSV file
 with open(budget_csv, newline='') as csvfile:
@@ -19,30 +21,30 @@ with open(budget_csv, newline='') as csvfile:
         # Count number of months
         total_months += 1
         # Get total amount of profit/losses
-        total_profit += int(row[1])
+        total_profit_loss += int(row[1])
         #Calculate change in profit/losses
-        profit_change = int(row[1]) - previous_profit
-        # Add change to list
-        profit_changes.append(profit_change)
+        profit_loss_change = int(row[1]) - prev_profit_loss
         # Set previous profit to current profit 
-        previous_profit = int(row[1])
+        prev_profit_loss = int(row[1])
 
-# Calculate average change in profit/losses
-average_change = sum(profit_changes) / len(profit_changes)
+# Find the greatest increase in profits (date and amount) over the entire period
+        if (profit_loss_change > greatest_increase[1]):
+            greatest_increase[0] = row[0]
+            greatest_increase[1] = profit_loss_change
 
-# Calculate greatest increase in profit and corresponding date
-greatest_increase = max(profit_changes)
-greatest_increase_date = str([profit_changes.index(greatest_increase) + 1][0])
+# Find the greatest decrease in losses (date and amount) over the entire period
+        if (profit_loss_change < greatest_decrease[1]):
+            greatest_decrease[0] = row[0]
+            greatest_decrease[1] = profit_loss_change
 
-# Calculate greatest decrease in profit and corresponding date
-greatest_decrease = min(profit_changes)
-greatest_decrease_date = str([profit_changes.index(greatest_decrease) + 1][0])
+# Calculate the average change in profit/loss over the entire period
+average_change = round(total_profit_loss/total_months, 2)
 
 # Print results
 print("Financial Analysis")
 print("--------------------")
 print(f"Total Months: {total_months}")
-print(f"Total: ${total_profit}")
-print(f"Average Change: ${round(average_change, 2)}")
-print(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})")
-print(f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})")
+print(f"Total: ${total_profit_loss}")
+print(f"Average Change: ${average_change}")
+print(f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})")
+print(f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})")        
